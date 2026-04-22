331c7fdfe462c27ed4685a527ba80826a8af67ce
최원영 <choewy32@gmail.com>
2026-04-22 12:20:32 +0900

    chore: typescript config(5.7 -> 6)

--- libs/persistence/src/mongoose/schemas/file.schema.ts
+++ libs/persistence/src/mongoose/schemas/file.schema.ts
@@ -7,25 +7,25 @@ export type FileDocument = HydratedDocument<File>;
 @Schema({ collection: 'fs.files' })
 export class File {
   @Prop({ type: Types.ObjectId, required: true })
-  _id: Types.ObjectId;
+  _id!: Types.ObjectId;
 
   @Prop({ type: Number, required: true })
-  length: number;
+  length!: number;
 
   @Prop({ type: Number, required: true })
-  chunkSize: number;
+  chunkSize!: number;
 
   @Prop({ type: Object, required: true })
-  metadata: { id: string; mimetype: string };
+  metadata!: { id: string; mimetype: string };
 
   @Prop({ type: Date, required: true })
-  uploadDate: Date;
+  uploadDate!: Date;
 
   @Prop({ type: String, required: true })
-  filename: string;
+  filename!: string;
 
   @Prop({ type: String, required: true })
-  ext: string;
+  ext!: string;
 }
 
 export const FileSchema = SchemaFactory.createForClass(File);
